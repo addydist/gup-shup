741a26e25721aa447ef628ad8aeb70c3d51bc922
addydist <adityapaljune@gmail.com>
2022-08-04 18:06:11 +0530

    intial commit

---
+++ public/src/components/Messages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+function Messages() {
+  return (
+    <Conatiner><div>Messages</div>
+  </Conatiner>
+    )
+
+}
+const Conatiner=styled.div``;
+export default Messages
---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome({currentUser}) {
+  return (
+    <Container>
+      <img src={Robot} alt="robot"/>
+      <h1>
+        Welcome,<span>{currentUser.Username}!!!!</span> 
+      </h1>
+      <h3>Select a chat to start messaging.........</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+color: white;
+flex-direction: column;
+img {
+  height: 20rem;
+}
+span {
+  color: #4e0eff;
+}
+`;
+
